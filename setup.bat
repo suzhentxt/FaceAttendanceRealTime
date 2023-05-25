ed34cf6010a9f8e39340431a6aac0d08ba2278b2
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-05-25 11:53:27 +0700

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+pip install setuptools==65.5.1
+pip install zipp==3.15.0
+pip install wheel==0.36.2
+pip install Pillow==9.5.0
+pip install cryptography==40.0.2
+pip install cmake==3.25.0
+pip install cvzone==1.5.6
+pip install dlib==19.24.0
+pip install face_recognition==1.3.0
+pip install firebase_admin==6.0.1
+pip install numpy==1.21.6
+pip install opencv-python==4.5.4.60
